bc63448ac20d22a126a7b3936d6c58edfe92ac00
sirlilpanda <sirlilpanda@gmail.com>
2022-06-04 00:33:22 +1200

    added a contuctor from vector

--- src/graphics_screen/char_pixel_screen.rs
+++ src/graphics_screen/char_pixel_screen.rs
@@ -1,17 +1,17 @@
 use std::fmt;
 
-use crate::char_pixel::CharPixel;
+use super::char_pixel::CharPixel;
 
-struct CharScreen {
+pub struct CharScreen {
     hight : u16,
     width : u16,
     screen : Vec<Vec<CharPixel>>
 }
 
-impl  CharScreen {
+impl CharScreen {
 
     pub fn new(temp_hight : u16, temp_width : u16) -> CharScreen{
-        let sc : Vec<Vec<CharPixel>> = vec![Vec::new()];
+        let mut sc : Vec<Vec<CharPixel>> = vec![Vec::new()];
         CharScreen {
             hight : temp_hight,
             width : temp_width,
@@ -20,6 +20,36 @@ impl  CharScreen {
 
     }
 
+    pub fn new_from_vecs(arr : Vec<Vec<(u8, u8, u8)>>) -> CharScreen{
+        //TODO add checks to make sure the arrays are of an even number
+        
+        let h: u16 = arr.len() as u16; 
+        let w: u16 = arr[1].len() as u16; 
+
+        let mut charscreen : Vec<Vec<CharPixel>> = vec![Vec::new()];
+
+        for i in 0..(h/2){
+            charscreen.push(Vec::new());
+            for j in 0..(w){
+                charscreen[i as usize].push(
+                    CharPixel::newfromtuple(
+                        arr[(i*2) as usize][j as usize], 
+                        arr[(i*2+1) as usize][j as usize]
+                    )
+                )
+                
+            }
+            
+        }
+        CharScreen { 
+            hight: h, 
+            width: w, 
+            screen: charscreen 
+        }
+    }
+
+
+
     pub fn string(&self) -> String{
         let mut s : String = String::new();
 
@@ -27,6 +57,7 @@ impl  CharScreen {
             for j in i.iter(){
                 s.push_str(j.asTrueColour().as_str())
             }
+            s.push_str(&"\n");
         }
         s
     }
