930aa14cd8e4970df9fc68e99fe04ef936fdb769
sirlilpanda <sirlilpanda@gmail.com>
2022-09-16 07:43:59 +1200

    minor changes

--- src/graphics_screen/char_pixel_screen.rs
+++ src/graphics_screen/char_pixel_screen.rs
@@ -3,26 +3,24 @@ use std::fmt;
 use super::char_pixel::CharPixel;
 
 pub struct CharScreen {
-    hight : u16,
-    width : u16,
-    screen : Vec<Vec<CharPixel>>
+    pub hight : u16,
+    pub width : u16,
+    pub screen : Vec<Vec<CharPixel>>
 }
 
 impl CharScreen {
 
     pub fn new(temp_hight : u16, temp_width : u16) -> CharScreen{
-        let mut sc : Vec<Vec<CharPixel>> = vec![Vec::new()];
         CharScreen {
             hight : temp_hight,
             width : temp_width,
-            screen : sc,
+            screen : Vec::new(),
         }
 
     }
 
     pub fn new_from_vecs(arr : Vec<Vec<(u8, u8, u8)>>) -> CharScreen{
         //TODO add checks to make sure the arrays are of an even number
-        
         let h: u16 = arr.len() as u16; 
         let w: u16 = arr[1].len() as u16; 
 
@@ -32,7 +30,7 @@ impl CharScreen {
             charscreen.push(Vec::new());
             for j in 0..(w){
                 charscreen[i as usize].push(
-                    CharPixel::newfromtuple(
+                    CharPixel::new_from_tuple(
                         arr[(i*2) as usize][j as usize], 
                         arr[(i*2+1) as usize][j as usize]
                     )
@@ -48,8 +46,7 @@ impl CharScreen {
         }
     }
 
-
-
+    //[TODO] update for relative printing
     pub fn string(&self) -> String{
         let mut s : String = String::new();
 
@@ -65,12 +62,7 @@ impl CharScreen {
 
 }
 
-
-
-
-
-
-impl   fmt::Display for CharScreen {
+impl fmt::Display for CharScreen {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f, "{}", self.string()
